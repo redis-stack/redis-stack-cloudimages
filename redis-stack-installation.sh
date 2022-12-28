d8453e4dbb478912b4a47b725eb074911f5cf26c
ofekshenawa <ofek.shenawa@redis.com>
2022-12-28 11:12:08 +0200

    change bin/bash to bin/sh

--- redis-stack-installation.sh
+++ redis-stack-installation.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
+set -x
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
